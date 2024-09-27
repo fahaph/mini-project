73152d0b12a67792bb60c93b0018e04fdc9df4cc
fahaph <F_Phonwen@hotmail.com>
2024-09-27 17:06:27 +0700

    pull data snapshot

--- account/lib/databases/transaction_db.dart
+++ account/lib/databases/transaction_db.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:account/models/transactions.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 // import 'package:sembast/sembast.dart';
@@ -9,7 +10,7 @@ class TransactionDB {
 
   TransactionDB({required this.dbName});
 
-  Future<Database> openDatabase() async{
+  Future<Database> openDatabase() async {
     Directory appDirectory = await getApplicationDocumentsDirectory();
 
     String dbLocation = join(appDirectory.path, dbName);
@@ -18,5 +19,39 @@ class TransactionDB {
     Database db = await dbFactory.openDatabase(dbLocation);
 
     return db;
+  }
+
+  Future<int> insertDatabase(Transactions transaction) async {
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory
+        .store('expense'); //สร้างตัวแปร ที่จะไปสร้าง db ที่ชื่อ expense
+
+    // json
+    var keyID = store.add(db, {
+      "title": transaction.title,
+      "amount:": transaction.amount,
+      "date": transaction.date.toIso8601String(),
+    });
+    db.close();
+    return keyID;
+  }
+
+  Future<List<Transactions>> loadAllData() async {
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store('expense');
+    var snapshot = await store.find(db);
+
+    // สร้างตัวแปรไว้เก็บข้อมูล transaction
+    List<Transactions> transactionList = [];
+
+    // ลูปเพิ่มข้อมูลจาก snapshot ไปยังตัวแปร transactionList
+    for (var record in snapshot) {
+      transactionList.add(Transactions(
+        title: record['title'].toString(),
+        amount: double.parse(record['amount'].toString()),
+        date: DateTime.parse(record['date'].toString()),
+      ));
+    }
+    return transactionList;
   }
 }
--- account/lib/provider/transaction_provider.dart
+++ account/lib/provider/transaction_provider.dart
@@ -1,6 +1,7 @@
 import 'package:account/databases/transaction_db.dart';
 import 'package:flutter/foundation.dart';
 import 'package:account/models/transactions.dart';
+// import 'package:account/databases/transaction_db.dart';
 
 class TransactionProvider with ChangeNotifier {
   List<Transactions> transactions = [];
@@ -10,8 +11,10 @@ class TransactionProvider with ChangeNotifier {
   }
 
   void addTransaction(Transactions transaction) async{
-    var db = await TransactionDB(dbName: 'transactions.db').openDatabase();
-    transactions.insert(0,transaction);
+    var db = await TransactionDB(dbName: 'transactions.db');
+    var keyID = await db.insertDatabase(transaction);
+    db.loadAllData();
+    print("keyID: $keyID");
     notifyListeners();
   }
 
